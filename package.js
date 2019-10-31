20241a9fc5b7ee3f01e051abeb76d9d7951da564
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-31 06:10:03 -0400

    fix to support wildcard route with no params

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteorhacks:picker',
   summary: 'Server Side Router for Meteor',
-  version: '1.0.5',
+  version: '1.0.6',
   git: 'https://github.com/meteorhacks/picker.git'
 });
 
